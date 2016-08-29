c81379ec5d45cb0b3cb24f1457e2fb73324ca3cc
ChenJunXu <466460684@qq.com>
2016-08-29 17:53:39 +0800

    add a test project

---
+++ MyApplication3/app/src/main/java/com/xu/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.xu.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xu.mylibrary.ToastUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ MyApplication3/mylibrary/src/main/java/com/xu/mylibrary/ToastUtils.java
@@ -0,0 +1,12 @@
+package com.xu.mylibrary;
+
+/**
+ * @author chenjunxu
+ * @date 16/8/29
+ */
+public class ToastUtils {
+
+    public static String getName() {
+        return "getName()";
+    }
+}
